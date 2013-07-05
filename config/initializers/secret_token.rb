c12f4c2c68a43afafc3c3576675f3bacd4245763
Mike Foley <foley3@gmail.com>
2013-07-05 13:36:37 -0700

    initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Validart::Application.config.secret_key_base = 'ba53e36c557ad6f69a0a1468c4dd40d6517ad6cb415cad467673775c4655351238a1139e59abffe17a109a92c14b8a6ed697301910ee9c480089a84e4fcaf96a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Validart::Application.config.session_store :cookie_store, key: '_validart_session'
